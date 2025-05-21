078dba36ea6824eec2335748c42699f3705283c1
IS <80646334+shariffi@users.noreply.github.com>
2025-05-21 15:42:22 +0100

    Fix Spline tool compatibility issues

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,6 @@ import react from '@vitejs/plugin-react';
 export default defineConfig({
   plugins: [react()],
   optimizeDeps: {
-    include: ['@splinetool/runtime']
-  }
+    exclude: ['lucide-react'],
+  },
 });
